10ae9bacd8f9a3bcd05ad14a003fa48158c54594
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-02-06 02:18:40 +0100

    Create View.py

---
+++ View.py
@@ -0,0 +1,16 @@
+class view:
+    def __init__(self):
+        #initialize variables
+
+    def input(self,):
+        #Take input sales command and put it into controller
+
+
+    def printCRM(self,):
+        #Define how to print aggregated values
+
+    def printReport(self):
+        #Define how to print aggregated values
+
+    def printCloseSales(self,):
+        #Define how to print aggregated values
