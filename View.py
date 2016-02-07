5c0bc04baf68ea0dac4d9e12201dbe3509c0b09c
jonasbeullens <jonas.beullens@gmail.com>
2016-02-07 17:25:34 +0100

    new viewtest

--- View.py
+++ View.py
@@ -1,16 +1,41 @@
-class view:
-    def __init__(self):
-        #initialize variables
+def inputCustid():
+    #Take input sales command and put it into controller
+    id = 1
+    newCust = input("is this a new customer (y/n)")
+    if('Y' == newCust.upper()):
+        name = input("enter the customer's name")
+        custid = id
+        id = id +1
+    else :
+        custid = input("Enter the customer's ID")
+        name = input("Enter the customer's name")
+    return custid, name
 
-    def input(self,):
-        #Take input sales command and put it into controller
+def inputSales():
+    sales = input("Enter the amount of sales in €")
+    return sales
+
+def inputSKU():
+    sku = input("Enter the SKU")
+    return sku
+
+def inputCC():
+    cc = 2
+    while(cc not in [0,1]):
+        cc = input("Cash (0) or CreditCard (1)?")
+        cc= int(cc)
+    return cc
 
 
-    def printCRM(self,):
-        #Define how to print aggregated values
+def printCRM(crm):
+    #Define how to print aggregated values
+    print(crm)
 
-    def printReport(self):
-        #Define how to print aggregated values
 
-    def printCloseSales(self,):
-        #Define how to print aggregated values
+def printReport(report):
+    #Define how to print aggregated values
+    print(report)
+
+def printCloseSales(closeSales):
+    #Define how to print aggregated values
+    print(closeSales)
