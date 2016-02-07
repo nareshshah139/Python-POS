787d0c2d69edcc4dfc77bfcffbdc2eb21cef91f1
ccbrandenburg <ccbrandenburg@gmail.com>
2016-02-07 17:34:27 +0100

    formatting

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 def inputCustid():
     #Take input sales command and put it into controller
     id = 1
-    newCust = input("is this a new customer (y/n)")
+    newCust = input("Is this a new customer (y/n): ")
     if('Y' == newCust.upper()):
-        name = input("enter the customer's name")
+        name = input("Enter the customer's name: ")
         custid = id
         id = id +1
     else :
-        custid = input("Enter the customer's ID")
-        name = input("Enter the customer's name")
+        custid = input("Enter the customer's ID: ")
+        name = input("Enter the customer's name: ")
     return custid, name
 
 def inputSales():
-    sales = input("Enter the amount of sales in €")
+    sales = input("Enter the amount of sales in €: ")
     return sales
 
 def inputSKU():
-    sku = input("Enter the SKU")
+    sku = input("Enter the SKU: ")
     return sku
 
 def inputCC():
     cc = 2
     while(cc not in [0,1]):
-        cc = input("Cash (0) or CreditCard (1)?")
+        cc = input("Cash (0) or CreditCard (1)? ")
         cc= int(cc)
     return cc
 
