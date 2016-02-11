d51d6c097c50043cff0c14cffc109a7a3558e1b3
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-02-11 16:46:07 +0100

    Update NewParser.py

--- NewParser.py
+++ NewParser.py
@@ -10,7 +10,50 @@ def main():
 		#Parser starts here
 		inputList = []
 		inputList = userinput.split()
+		#Basic idea: Look for CustID as a alphanumeric in the list. If the list finds an alphanumeric, look for a number 
+		#in the list which is definitely the Sale value. 
+		#
 		for element in inputList:
+			if inputList[element].isalnum():
+				for element2 in inputList:	
+					if inputList[element2].isnumeric():
+						model.SetSales(inputList[element],float(inputList[element2]))
+					elif inputList[element2].lower() == 'CC':
+						model.SetCC(inputList[element],inputList[element2])
+					elif inputList[element2].startswith('SKU:'):
+						model.SetSKU(inputList[element],float(inputList[element2].split(':')[1]))
+					elif inputList[element2].isalpha():
+						model.SetCustomerName(inputList[element],inputList[element2]
+					else:
+						view.printError()
+			else:
+				if inputList[element].isnumeric():
+					model.SetSales('Anon12345',float(inputList[element]))
+				elif inputList[element] == 'CC':
+					model.SetCC('Anon12345',inputList[element])
+				elif inputList[element2].startswith('SKU:'):
+					model.SetSKU('Anon12345',float(inputList[element].split(':')[1]))
+				elif inputList[element].isalpha():
+					model.SetCustomerName('Anon12345',inputList[element]
+				elif inputList[element].lower() == 'close day':
+					closeday()
+				#Checks if the user wants to print the sales for that day with cash or CC breakdwon
+				#Example command: report
+				elif inputList[element].lower() == 'report':
+					ReportCall1()
+				#Checks if the user want to print the sale for that day by client
+				#Example command: crm
+				elif inputList[element].lower() == 'crm':
+					CRM()
+				#Example command: help
+				elif inputList[element].lower() == 'help':
+					view.printCommandList()
+				else:
+					view.printError()
+			
+			
+					
+						
 		  
 
 def ReportCall1():
