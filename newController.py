9b44c5c1db65de56610fcde8ec887111e10d58b7
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-02-11 16:00:07 +0100

    Update newController.py
    Provided Examples...

--- newController.py
+++ newController.py
@@ -13,18 +13,27 @@ def main():
 		#Checks if the user started by inputing the sale price
 		if inputList[0] == 'Sale':
 			#Checks if the user inputed two values
+			#This is for commands like Sale SKU:numeric and Sale Numeric
+			
 			if len(inputList) == 2:
 				#If second entry is numeric it's the sale price
+				#Specifically for the Sale Numeric command
+				# Example: Sale 100
 				if inputList[1].isnumeric():
 					model.setSales('Anon12345',float(inputList[1]))
 					model.setCustID('Anon12345')
 				#If second entry is alphanumeric it's the SKU
+				#Specifically for the Sale SKU:numeric command
+				# Example command: Sale SKU:12345
 				elif inputList[1].isalnum():
 					model.setSKU('Anon12345',float(inputList[1].split(':')[1]))
 					model.setCustID('Anon12345')
 				else:
 					view.printError()
 			#Checks if the user inputed three values
+			#Works with commands like Sale CustID CC, Sale Numeric CustomerID
+			#Example commands: Sale Anon12345 CC(this is in $) Separated concern since this stores differently.
+			#Example commands: Sale 100 Anon12345
 			elif len(inputList) == 3:
 				#If third entry is CC adds it to the Credit Card count
 				if inputList[2] == 'CC':
@@ -42,19 +51,24 @@ def main():
 			else:
 				view.printError()
 		#Checks if the user started by inputing the customer name
+		# Example command: customer Naresh Anon12341
 		elif inputList[0].lower() == 'customer':
 			#assings the third value as customer id
 			model.setCustID(inputList[2])
 			model.setCustomerName(inputList[2],inputList[1])
 		#Checks if the user wants to close the sales for that day
+		#Example command: close day
 		elif inputList[0].lower() == 'close day':
 			closeday()
 		#Checks if the user wants to print the sales for that day with cash or CC breakdwon
+		#Example command: report
 		elif inputList[0].lower() == 'report':
 			ReportCall1()
 		#Checks if the user want to print the sale for that day by client
+		#Example command: crm
 		elif inputList[0].lower() == 'crm':
 			CRM()
+		#Example command: help
 		elif inputList[0].lower() == 'help':
 			view.printCommandList()
 		else:
