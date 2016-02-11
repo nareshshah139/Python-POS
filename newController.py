ed1f62b5501f5cf6626db3bd0c58e1ea4c92edcd
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-02-11 06:24:38 +0100

    Update newController.py
    Added a parser and defined some additional helpful functions. Lets get working on this quickly.

--- newController.py
+++ newController.py
@@ -1,25 +1,56 @@
-import random
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 05:50:30 2016
+
+@author: nareshshah
+"""
+
 import ViewOld as view
 import Model as model
 
 def main():
 	temp = 0
 	while(temp!=1):
-		CustID = view.inputCustID()
-		model.setCustID(CustID)
-		model.setCustomerNAME(CustID, view.inputName())
-		model.setSKU(CustID, view.inputSKU())
-		model.setCC(CustID,view.inputCC())
-		model.setSales(CustID, view.inputSales())
-		
-		
-		
+		userinput = input("Please Enter a command")
+		#Parser starts here
+		inputList = []
+		inputList = userinput.split()
+		if inputList[0] == 'Sale':
+			if inputList.length == 2:
 				
+				if inputList[1].isnumeric():
+					model.setSales(float(inputList[1]))
+				
+				elif inputList[1].isalnum():
+					model.setSKU(float(inputList[1].split(':')[1]))
 					
+			elif inputList.length == 3: 
 				
-	
-
-        
+				if inputList[2] == 'CC':
+					model.setCC(inputList[2])
+					model.setSales(inputList[1])
+				elif inputList[2].isalnum():
+					model.setSales(inputList[1])
+					model.setCustID(inputList[2])
+				else: 
+					view.printError()
+			else:
+				view.printError()
+			
+		elif inputList[0] == 'Customer':
+			model.setCustID(inputList[2])
+			model.setCustomerName(inputList[2],inputList[1])
+		elif inputList[0] == 'Close day':
+			closeday()
+		elif inputList[0] == 'Report':
+			ReportCall1()
+		elif inputList[0] == 'CRM':
+			CRM()
+		elif inputList[0] == 'Help':
+			view.printCommandList()
+		else:
+			view.printError()
+				
 
 def ReportCall1():
         #Call the Get functions from model and use the view functions to print a report
