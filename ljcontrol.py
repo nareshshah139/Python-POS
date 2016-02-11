42a52a9ae564f5abd79dfbf3d3f8d47e85b1e1ec
ljohnnes <ljohnnes@gmail.com>
2016-02-11 20:46:32 +0100

    if elif options TO START

---
+++ ljcontrol.py
@@ -0,0 +1,46 @@
+# ljcontrol.py
+
+# sale 300 CC SKU:alpha1234 ID: 9alphanum
+
+import random
+import view
+import model
+
+def main():
+	temp = 0
+	while (temp!=1):
+		userpinput = input("Please submit the sale information > ")
+
+		inputList = []
+		inputList = userinput.split()
+
+		# if/else check for sale / customer
+		# if s
+
+
+
+
+
+
+if "sale" in input.lower():
+	# enter sale info
+
+
+
+
+
+
+elif "customer" in input.lower():
+	# enter customer id
+
+elif "crm" in input.lower():
+	# call 
+
+elif "report" in input.lower():
+	# call report function
+
+elif "close day" in input.lower():
+	# call close day function
+
+else:
+	# print error
