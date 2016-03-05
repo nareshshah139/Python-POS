05283b306f516b6302877d785debe9cc5fbceb37
ljohnnes <ljohnnes@gmail.com>
2016-03-05 12:18:29 +0100

    Modeldb minor edit

--- modeldb.py
+++ modeldb.py
@@ -5,29 +5,20 @@ import sqlite3
 # 
 conn = sqlite3.connect('pos.db')
 c = conn.cursor()
-c.execute('CREATE TABLE sales (SaleID INTEGER, CustID VARCHAR(9), CC BOOLEAN, SKU VARCHAR(9), SALES FLOAT(9), Day DATE) IF NOT EXISTS sales')
-c.execute('CREATE TABLE items (SKU VARCHAR(9), SALES FLOAT(9), Day DATE) IF NOT EXISTS items')
+c.execute('CREATE TABLE items (SKU VARCHAR(9), ITEM VARCHAR(50)) IF NOT EXISTS items')
+c.execute('CREATE TABLE sales (SaleID INTEGER, CustID VARCHAR(9), CC BOOLEAN, SKU VARCHAR(9), SALES FLOAT(9), Day DATE PRIMARY KEY (SaleID) FOREIGN KEY SKU REFERENCES items SKU) IF NOT EXISTS sales')
 
 
-
-
-
-# Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
 
 # Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
-conn.commit()
+#conn.commit()
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-conn.close()
+#conn.close()
 
 
 
-cur.execute('DROP TABLE IF EXISTS Tracks ')
-cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
-conn.close()
