e82e0efd6a6db7064fc8fde3ef8e040f609ea847
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-03-05 17:13:08 +0100

    Added files via upload

--- Notebook1.py
+++ Notebook1.py
@@ -1,5 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 import time
 
 
@@ -19,6 +23,9 @@ n = ttk.Notebook(master, name = 'n')
 CC = IntVar()
 Cash = IntVar()
 
+def callback():
+    print('Button Clicked')
+
 
 ##        f1 = ttk.Frame(n)   # first page, which would get widgets gridded into it
 ##        f2 = ttk.Frame(n)   # second page
@@ -30,10 +37,12 @@ CustomerView = ttk.Frame(n)
 SalesView = ttk.Frame(n)
 POSView = ttk.Frame(n)
 ReportView = ttk.Frame(n)
-n.add(CustomerView, text = 'Customer View')
+CRMView = ttk.Frame(n)
+n.add(POSView, text = 'Pont of Sale System View')
 n.add(SalesView, text = 'Sales View')
-n.add(POSView,text = 'Point of Sale System View')
+n.add(CustomerView,text = 'New Customer View')
 n.add(ReportView, text = 'Report View')
+n.add(CRMView, text = 'CRM View')
 n.pack(fill = BOTH,expand = TRUE)
 
 
@@ -49,8 +58,11 @@ C_Name.pack(fill = X,padx = 10, expand = TRUE)
 f2_CV = Frame(CustomerView)
 f2_CV.pack(fill = X)
 Label(f2_CV,text = 'Customer ID').pack(side = LEFT)
-C_ID = Entry(f2_CV)
-C_ID.pack(fill = X,padx = 5,expand = TRUE)
+
+
+f3_CV = Frame(CustomerView)
+f3_CV.pack(fill = X)
+B_Add_User = Button(f3_CV,text = 'Add User', command = callback).pack()
 
 # POS View Tab
 f1_POS = Frame(POSView)
@@ -83,10 +95,53 @@ f5_POS.pack(fill=X)
 Label(f5_POS,text = 'Current Date').pack(side = LEFT)
 Label(f5_POS,text = time.strftime("%d/%m/%Y")).pack(side = LEFT, padx = 5)
 
+f6_POS = Frame(POSView)
+f6_POS.pack(fill = X)
+B_POS_Submit = Button(f6_POS,text = 'Submit', command = callback).pack()
 
 
 
+#Report View
+f1_RV = Frame(ReportView)
+f1_RV.pack(fill=X)
+Label(f1_RV,text = 'Report and Graphs').pack(padx = 10,pady=10)
 
+f2_RV = Frame(ReportView)
+f2_RV.pack(fill=BOTH, expand = TRUE)
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+canvas = FigureCanvasTkAgg(f, f2_RV)
+canvas.show()
+canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+toolbar = NavigationToolbar2TkAgg(canvas, f2_RV)
+toolbar.update()
+canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+
+#CRM View
+f1_CRM = Frame(CRMView)
+f1_CRM.pack(fill = X)
+Label(f1_CRM,text = 'CRM Reports').pack(padx = 10, pady = 10)
+
+f2_CRM = Frame(CRMView)
+f2_CRM.pack(fill = BOTH, expand = TRUE)
+f = Figure(figsize = (5,5),dpi = 100)
+a = f.add_subplot(111)
+a.plot([range(10)],[range(10)])
+
+##graphArray = {'SalesID':[1,2,3,4,5,6,7,8,9,10],
+##              'CustID':['123','124','125','122','123','124','125','122'],
+##              'Name':['Chris','Chris','Lionel','Jonas','Fotis','Chris'
+##                      'Chris','Lionel','Jonas','Fotis'],
+##              'Payment':[
+
+canvas = FigureCanvasTkAgg(f, f2_RV)
+canvas.show()
+canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+toolbar = NavigationToolbar2TkAgg(canvas, f2_RV)
+toolbar.update()
+canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
         
 ##class CustomerView(tk.Frame):
 ##
