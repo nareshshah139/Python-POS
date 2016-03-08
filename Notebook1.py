9958a5b62ca2ec148c4a810c513af4106c9f5f88
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-03-08 09:58:47 +0100

    Added files via upload

--- Notebook1.py
+++ Notebook1.py
@@ -8,12 +8,19 @@ import time
 import pandas as pd
 import numpy as np
 import Controllerv2
+import matplotlib.pyplot as plt
+##from PIL import Image, ImageTk
+plt.style.use('ggplot')
+##
+##image = Image.open('Dilemma.jpg')
+##photo = ImageTk.PhotoImage(image)
 
-
-## Creating Master Frame.
+## Creating Master Frame. Naming Master Frame as root.
 root = Tk()
+## Creating a frame inside master frame called master.
 master = Frame(root, name = 'master')
 master.pack(fill = BOTH)
+#Setting display title to be equal to 'Point of Sale System'
 root.title('Point of Sale System')
 #root.protocol('WM_DELETE_WINDOW',master.quit)
 n = ttk.Notebook(master, name = 'n')
@@ -31,29 +38,30 @@ def load_data_sql():
 
 
 
-def callback():
-    print('Button Clicked')
-
 def callback1():
+    '''Callback function to push values from GUI to controller'''
     list1 = [C_ID.get(),CC_Var.get(),SKU.get(),Sales.get(),Date_POS_Var]
     print(list1)
     Controllerv2.newsalebutton(list1)
 
 def callback2():
+    '''Callback function for adding new user to the database'''
     list2 = [C_Name.get(), Date_CV_Var]
     print(list2)
     Controllerv2.newcustbutton(list2)
 
 def sel():
+    '''Callback function for radiobutton to return values'''
     CC_1 = CC_Var.get()
     
 
 def printerrors():
+    '''Callback function to check values input in GUI for errors and return a printed value'''
     Controllerv2.allerrors()
 
 
 
-
+#Placeholder Data for Fotis and Chris
 graphArray = {'SalesID': [1,2,3,4,5,6,7,8,9,10],
 'CustID':['123','123','124','125','122','123','123','124','125','122'],
 'Name':['Chris','Chris','Lionel','Jonas','Fotis','Chris','Chris','Lionel','Jonas','Fotis'],
@@ -71,11 +79,10 @@ transaction_data = {'SalesID': [1,2,3,4,5,6,7,8,9,10],
 'Sales':[20,30,30,40,20,20,30,30,40,30],
 'Date': ['2015-01-01', '2015-01-02', '2015-01-03', '2015-01-04','2015-01-05', '2015-01-05', '2015-01-06', '2015-01-07','2015-01-08', '2015-01-09']}
 
-df5 = pd.DataFrame(graphArray)
-df2 = df5.groupby('Name')['Sales'].sum()
-df3 = pd.DataFrame({'Name':df2.index, 'TotalSales':df2.values})
+
 
 def create_df():
+    ''' Creates Dataframe from the Data collected in the database '''
     global df
     df = pd.DataFrame(transaction_data)
     df['CC'] = df['Payment']==1
@@ -87,6 +94,7 @@ def create_df():
 #Top Customer Table
 #Group dataframe by CustID and take sum of sales
 def top_customers():
+    '''Returns a dataframe of highest paying customers in the Database'''
     df_sales = df[['CustID','Name','Sales']].groupby(['CustID','Name']).agg([np.sum, np.count_nonzero])
     df_sales.columns = ['SalesAmount','ItemCount']
     # Sort by sum of sales in descending order
@@ -96,6 +104,7 @@ def top_customers():
 
 #Top SKU Table
 def top_sku():
+    '''Returns a dataframe of the highest selling items in the product database '''
     df_topSKU = df[['SKU','Sales']].groupby(['SKU']).agg([np.sum, np.count_nonzero])
     df_topSKU.columns=['SalesAmount','SalesCount']
     df_topSKU_sort = df_topSKU.sort_values(['SalesAmount'], ascending=False)
@@ -104,20 +113,16 @@ def top_sku():
 #PLOTTING
 #Create Sales by Day Table
 def sales_overview():
+    ''' Defines Daily sales variables. This allows us to plot graphs '''
     global daily_sales
     daily_sales = df[['Date','Sales','CCSales','CashSales']].groupby(['Date']).agg([np.sum])
     daily_sales.columns = ['TotalSales','CCSales','CashSales']
     return daily_sales
 
-#create plot
-##def sales_plot():
-##    global plot1
-##    plot1 = daily_sales.plot(title="Sales by Payment Method", xticks=df['Day'],lw=2,colormap='gnuplot',marker='.',markersize=10)
-##    plot1.set(xlabel="Date", ylabel="Sales Amount")
-##    return plot1
 
 #Create Sales by SKU Table
 def sku_overview():
+    ''' Defines SKU Sales variable. This allows us to plot graphs '''
     global sku_sales
     sku_sales = df[['SKU','CCSales','CashSales']].groupby(['SKU']).agg([np.sum])
     sku_sales.columns = ['CCSales','CashSales']
@@ -131,6 +136,12 @@ def sku_overview():
 
 
 ##---- Fotis -----
+# Setting the Frames of the notebook in the following order:
+# 1 - POS View
+# 2 - Sales View
+# 3 - Customer View - To Add new customers
+# 4 - Report View - To plot Report graphs
+# 5 - CRM View - To plot CRM graphs
 
 CustomerView = ttk.Frame(n)
 SalesView = ttk.Frame(n)
@@ -175,7 +186,7 @@ B_Add_User = Button(f3_CV,text = 'Add User', command = callback2).pack(side = RI
 # Make a frame to hold all elements
 # Make smaller frames in order to hold pairs of elements per line.
 # Add buttons with default callback. Callback can be changed to a different function
-# CC Variable is CC
+# CC Variable is CC_Var
 # CustomerID Variable is C_ID_Var
 # SKU Variable is SKU_Var
 # Sales Variable is Sales_Var
@@ -191,8 +202,6 @@ C_ID_Var = C_ID.get()
 f2_POS = Frame(POSView)
 f2_POS.pack(fill = X)
 Label(f2_POS,text = 'Credit Card').pack(side = LEFT)
-#CC = IntVar()
-#Checkbutton(f2_POS, text="Yes?", variable=CC).pack(side = LEFT)
 CC_Var = IntVar()
 CC = Radiobutton(f2_POS, text="CC", variable=CC_Var, value=1,
                   command=sel).pack(side = LEFT)
@@ -224,6 +233,11 @@ f6_POS.pack(fill = X)
 B_POS_Submit = Button(f6_POS,text = 'Submit', command = callback1).pack()
 
 
+
+##f7_POS = Frame(POSView)
+##f7_POS.pack(fill=BOTH,expand = TRUE)
+##Label(f7_POS,image=photo).pack()
+
 #f7_POS = Frame(POSView)
 #f7_POS.pack(fill=X)
 #Label(f7_POS, text = printerrors()).pack(side= LEFT)
@@ -233,6 +247,12 @@ B_POS_Submit = Button(f6_POS,text = 'Submit', command = callback1).pack()
 
 
 #Report View
+#Plotting works like this
+# 1. Create a figure in a frame using f = Figure(figsize(5,5), dpi = 100)
+# 2. Set the axes for the figures ax1 = f.add_subplot(111) for example
+# 3. Plot the graph mentioning where you want the graph to be placed by
+#    mentioning the correct axis
+
 f1_RV = Frame(ReportView)
 f1_RV.pack(fill=X)
 Label(f1_RV,text = 'Report and Graphs').pack(padx = 10,pady=10)
@@ -268,10 +288,11 @@ Label(f1_CRM,text = 'CRM Reports').pack(padx = 10, pady = 10)
 f2_CRM = Frame(CRMView)
 f2_CRM.pack(fill = BOTH, expand = TRUE)
 f1 = Figure(figsize = (5,5),dpi = 100)
-ax2 = f1.add_subplot(211)
-ax3 = f1.add_subplot(212)
+ax2 = f1.add_subplot(111)
+df5 = pd.DataFrame(graphArray)
+df2 = df5.groupby('Name')['Sales'].sum()
+df3 = pd.DataFrame({'Name':df2.index, 'TotalSales':df2.values})
 df3.plot(x = 'Name', y= 'TotalSales',kind = 'bar', ax = ax2)
-df3.plot(x = 'Name',y = 'TotalSales',kind = 'bar',ax=ax3)
 
 canvas = FigureCanvasTkAgg(f1, f2_CRM)
 canvas.show()
