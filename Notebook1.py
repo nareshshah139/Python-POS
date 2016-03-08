af72e94930fc023baf6a5c2b84b2b857b3f02dd1
jonasbeullens <jonas.beullens@gmail.com>
2016-03-08 12:08:31 +0100

    h

--- Notebook1.py
+++ Notebook1.py
@@ -25,7 +25,7 @@ root.title('Point of Sale System')
 #root.protocol('WM_DELETE_WINDOW',master.quit)
 n = ttk.Notebook(master, name = 'n')
 
-
+"""
 # SHOULD NOT GO IN THE GUI
 # CONSISTENCY -- THIS AND/OR FETCH?
 #-----Fotis-----
@@ -34,8 +34,9 @@ def load_data_sql():
     df = pd.read_sql_query("SELECT * FROM sales, customers WHERE sales.CustIDcol=customers.CustIDcol",connection)
     return(df)
 
-#load_data_sql()
+#load_data_sql()    THIS FUNCTION CAN BE CALLED THROUGH =>    Modelv2.POS.getPosData()  BUT WHERE??
 
+"""
 
 
 def callback1():
