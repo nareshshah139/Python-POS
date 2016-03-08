dff89b3964e96731cbf09e5a6bb23d0d6ade1cf2
ljohnnes <ljohnnes@gmail.com>
2016-03-08 23:06:46 +0100

    Revert "check cust tuples fix attempt"
    This reverts commit 69d9bab604f8e9beaadc88e516b4c397d29d6c64, reversing
    changes made to 17e9029873ec055d12c33c41aea49d836d367c01.

--- modeldb.py
+++ modeldb.py
@@ -2,8 +2,6 @@
 
 import sqlite3
 import time
-import pandas as pd
-import os
 
 #Start by connecting to the pos.db database
 conn = sqlite3.connect('pos.db')
@@ -71,17 +69,15 @@ class Customer(object):
 	def checkCust():
 		c.execute('''SELECT CustIDcol FROM customers''')
 		custIDtuple = c.fetchall()
+		print("Model tuples",custIDtuple)
 		CustIDlist = []
 		try:
 			for i in custIDtuple:
 				CustIDlist.append(i[0])
-# checking if the new list prints:
 			print("Model list", custIDlist)
 			return custIDlist
-#			return custIDtuple
 		except:
 			print("why not?")
-
 
 
 class Product(object):
@@ -143,9 +139,9 @@ class POS(object):
 
 	@staticmethod
 	def getPosData():
-		c.execute("SELECT * FROM sales")
-		sales_data = c.fetchall()
-		return(sales_data)
+		c.execute("SELECT * FROM sales, customer LEFT JOIN sales.CustIDcol, customers.CustIDCol")
+		df = c.fetchall()
+		return(df)
 
 	@staticmethod
 	def CRM():
@@ -163,9 +159,5 @@ def closeDay():
 
 
 
-def write_data_sql():
-    sales_table = pd.DataFrame.from_csv(os.getcwd()+"/sales.csv",sep=";",header=0)
-    customer_table = pd.DataFrame.from_csv(os.getcwd()+"/customers.csv",sep=";",header=0) #specify path
-    sales_table.to_sql('sales', conn, if_exists='append')
-    customer_table.to_sql('customers', conn, if_exists='append')
+
 
